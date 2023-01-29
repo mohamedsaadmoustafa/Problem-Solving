fe788c902b42705fb105e7b762222a272fddccbc
Mohamed Saad <mohamedsaadmoustafa@gmail.com>
2023-01-29 08:37:30 +0200

    Update 00136. Single Number.py

--- LeetCode/00136. Single Number.py
+++ LeetCode/00136. Single Number.py
@@ -1,3 +1,28 @@
+# another solution
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        temp = []
+        for num in nums:
+            if num in x:
+                temp.remove(num)
+            else:
+                temp.append(num)
+        return temp[0]
+
+# Another Solution
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return 2*sum(set(nums))-sum(nums)
+    
+# Better Solution
 class Solution:
     def singleNumber(self, nums) -> int:
         if len(nums) == 1: return nums[0]
@@ -7,5 +32,4 @@ class Solution:
                 d[num] += 1
             else:
                 d[num] = 1
-                
         return min(d, key=d.get)
