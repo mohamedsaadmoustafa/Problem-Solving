bcc51cde597b9fe0ece2ab2fb2e5cd229ccdd799
Mohamed Saad <mohamedsaadmoustafa@gmail.com>
2023-01-29 19:58:22 +0200

    Update 00205. Isomorphic Strings.py

--- LeetCode/00205. Isomorphic Strings.py
+++ LeetCode/00205. Isomorphic Strings.py
@@ -3,7 +3,7 @@ class Solution:
         map, reverse_map = {}, {}
 
         for ss, tt in zip(s, t):
-            if (ss in map and map[ss] != tt) or ((tt in reverse_map and map[tt] != ss)): 
+            if (ss in map and map[ss] != tt) or ((tt in reverse_map and reverse_map[tt] != ss)): 
                 return False
             map[ss], reverse_map[tt] = tt, ss
         return True
