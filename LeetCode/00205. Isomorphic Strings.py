f0ec3ff3984e78ef9407113895fd13d3e88201d4
Mohamed Saad <mohamedsaadmoustafa@gmail.com>
2023-01-29 19:53:36 +0200

    Create 00205. Isomorphic Strings.py

---
+++ LeetCode/00205. Isomorphic Strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        map, reverse_map = {}, {}
+
+        for ss, tt in zip(s, t):
+            if (ss in map and map[ss] != tt) or ((tt in reverse_map and map[tt] != ss)): 
+                return False
+            map[ss], reverse_map[tt] = tt, ss
+        return True
