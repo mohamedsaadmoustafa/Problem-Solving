642e9658f94e28ed3d892b44edefe0b53e667c70
Mohamed Saad <mohamedsaadmoustafa@gmail.com>
2023-03-30 05:20:54 +0200

    Update 00189. Rotate Array.py

--- LeetCode/00189. Rotate Array.py
+++ LeetCode/00189. Rotate Array.py
@@ -5,4 +5,5 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        k %= len(nums)
         nums[:] = nums[-k:] + nums[:-k]
