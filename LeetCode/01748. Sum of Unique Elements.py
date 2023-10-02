2bfc79ec9926ead570790901451eb1453c6435c9
Mohamed Saad <mohamedsaadmoustafa@gmail.com>
2023-10-02 23:44:25 +0300

    Update 01748. Sum of Unique Elements.py

--- LeetCode/01748. Sum of Unique Elements.py
+++ LeetCode/01748. Sum of Unique Elements.py
@@ -8,3 +8,15 @@ class Solution:
             if num not in nums[index+1:] and num != nums[index-1]:
                 sum += nums[index]
         return sum
+
+
+# another solution
+from collections import defaultdict
+
+class Solution:
+    def sumOfUnique(self, nums: List[int]) -> int:
+        counts = defaultdict(int)
+        for num in nums:
+            counts[num] += 1
+
+        return sum(key for key in counts if counts[key] == 1)
